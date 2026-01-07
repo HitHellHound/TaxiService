a6a9a677d41c2eaf6bffe42946e3638c95e51209
HitHellHound <i.ignatch.com@mail.ru>
2026-01-07 12:11:51 +0300

    TG Notification service init

---
+++ tg-notification/src/main/java/com/efcon/tg_notification/TgNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.efcon.tg_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgNotificationApplication.class, args);
+	}
+
+}
---
+++ tg-notification/src/test/java/com/efcon/tg_notification/TgNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efcon.tg_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
